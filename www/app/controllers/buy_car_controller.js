9b9492b4804cacdf949f8f498bcd3d89d7bdf68a
Dmitry <zimokk1@gmail.com>
2016-05-18 22:24:02 +0200

    Added cities factory to controller

--- www/app/controllers/buy_car_controller.js
+++ www/app/controllers/buy_car_controller.js
@@ -1,6 +1,7 @@
-app.controller('BuyCarCtrl', ['$scope','Models','Marks', 'Countries', 'Regions',function($scope,Models,Marks,Countries,Regions) {
+app.controller('BuyCarCtrl', ['$scope','Models','Marks', 'Countries', 'Regions','Cities',function($scope,Models,Marks,Countries,Regions,Cities) {
     $scope.Models = Models.all;
     $scope.Marks = Marks.all;
     $scope.Countries = Countries.all;
     $scope.Regions = Regions.all;
+    $scope.Cities = Cities.all;
 }]);
