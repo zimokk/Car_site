30326e304dc8018fdbed63c0e31c4e33d62c7f06
Dmitry <zimokk1@gmail.com>
2016-05-19 19:26:11 +0200

    Edited car block view

--- www/app/controllers/directives_controllers/car_block_controller.js
+++ www/app/controllers/directives_controllers/car_block_controller.js
@@ -1,6 +1,6 @@
-app.controller('carBlockController',["$scope", 'Marks', 'Models', 'Fuels', function($scope,Marks,Models,Fuels) {
-    $scope.Marks = Marks;
+app.controller('carBlockController',["$scope", 'Marks', 'Models', 'Fuels', 'Bodies', function($scope,Marks,Models,Fuels,Bodies) {
     $scope.mark_name = Marks.getById($scope.car.mark_id).name;
     $scope.model_name = Models.getById($scope.car.model_id).name;
     $scope.fuel_name = Fuels.getById($scope.car.fuel_id).name;
+    $scope.body_name = Bodies.getById($scope.car.body_id).name;
 }]);
--- www/app/servecies/bodies_service.js
+++ www/app/servecies/bodies_service.js
@@ -5,6 +5,16 @@ app.factory('Bodies',function($http){
             $http.get("php/read_bodies.php").success(function(response){
                 angular.copy(response.bodies,bodiesService.all);
             });
+        },
+        getById: function(id){
+            var item;
+            bodiesService.all.forEach(function(body,number){
+                if(body.id == id){
+                    item = body;
+                    return item;
+                }
+            },id);
+            return item;
         }
     };
     bodiesService.getAll();
