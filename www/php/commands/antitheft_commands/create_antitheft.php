718844656a172c564521d8388f487da7dc80f34f
Dmitry <zimokk1@gmail.com>
2016-06-12 21:20:48 +0200

    Added php-command create antitheft

---
+++ www/php/commands/antitheft_commands/create_antitheft.php
@@ -0,0 +1,49 @@
+<?php
+// include database and object files
+include_once '../../config/database.php';
+include_once '../../objects/antitheft.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$antitheft = new Antitheft($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+$antitheft->immobilizer = $data->immobilizer;
+$antitheft->signaling = $data->signaling;
+
+// create the product
+
+if($antitheft->create()){
+    $stmt = $antitheft->readByParams();
+    $num = $stmt->rowCount();
+
+    $data="";
+
+    if($num>0){
+
+        $x=1;
+
+        // retrieve our table contents
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+
+            extract($row);
+
+            $data .= '{';
+                $data .= '"id":"'  . $id . '",';
+                $data .= '"immobilizer":"'  . $immobilizer . '",';
+                $data .= '"signaling":"' . $signaling . '"';
+            $data .= '}';
+
+            $data .= $x<$num ? ',' : ''; $x++; }
+    }
+
+    echo '{"antithefts":[' . $data . ']}';
+}
+
+// if unable to create the product, tell the user
+else{
+    echo "Unable to create antitheft.";
+}
+?>
