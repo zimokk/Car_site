8c578e23e6e2ffe4a1d8c778fa9cdb6fbbc1c453
Dmitry <zimokk1@gmail.com>
2016-07-08 18:27:34 +0200

    Added deleteImage button & method handler

--- www/app/directives/image_preview_directive.js
+++ www/app/directives/image_preview_directive.js
@@ -2,11 +2,11 @@ app.directive('imagePreviewDiv', function() {
     return {
         restrict: 'E',
         scope: {
-            image: '='
+            image: '=',
+            remove: "&"
         },
         templateUrl: 'app/templates/directive_templates/image_preview_template.html',
         link: function (scope) {
-            scope.image;
         }
     };
 });
