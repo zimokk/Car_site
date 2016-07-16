50bde4c163af01a681301c03938e4af105b823bc
Dmitry <zimokk1@gmail.com>
2016-07-16 18:28:53 +0200

    Added client-side email & login validations

--- www/app/controllers/registration_controller.js
+++ www/app/controllers/registration_controller.js
@@ -1,7 +1,20 @@
 app.controller('RegistrationCtrl', ['$scope','Users',function($scope, Users) {
+    $scope.Users = Users;
+    $scope.registration_validation = Users.registration_validation;
     $scope.authorize = function(){
         var enter_login = $scope.enter_login;
         var enter_password = $scope.enter_password;
         Users.authorize(enter_login, enter_password);
+    };
+    $scope.checkEmail = function(){
+        var email = $scope.registration_email;
+        Users.checkEmail(email);
+    };
+    $scope.checkLogin = function(){
+        var login = $scope.registration_login;
+        Users.checkLogin(login);
+    };
+    function setEmailError(reason){
+        alert(reason);
     }
 }]);
--- www/app/servecies/users_service.js
+++ www/app/servecies/users_service.js
@@ -2,6 +2,7 @@ app.factory('Users',function($http){
     var usersService = {
         all: [],
         current: {},
+        registration_validation: new registration_validation(),
         getAll: function() {
             $http.get("php/commands/users_commands/read_users.php").success(function(response){
                 angular.copy(response.users,usersService.all);
@@ -16,6 +17,44 @@ app.factory('Users',function($http){
             }).error(function(msg){
 
             });
+        },
+        checkEmail: function(email){
+            if(email != null && email != ""){
+                $http.post('php/commands/users_commands/check_email.php', {
+                    'email' : email
+                }).success(function(response){
+                    if(response.users.length == 1){
+                        usersService.registration_validation.setEmailValidation(false);
+                    }
+                    else{
+                        usersService.registration_validation.setEmailValidation(true);
+                    }
+                }).error(function(msg){
+
+                });
+            }
+            else{
+                usersService.registration_validation.setEmailValidation(false);
+            }
+        },
+        checkLogin: function(login){
+            if(login != null && login != ""){
+                $http.post('php/commands/users_commands/check_login.php', {
+                    'login' : login
+                }).success(function(response){
+                    if(response.users.length == 1){
+                        usersService.registration_validation.setLoginValidation(false);
+                    }
+                    else{
+                        usersService.registration_validation.setLoginValidation(true);
+                    }
+                }).error(function(msg){
+
+                });
+            }
+            else{
+                usersService.registration_validation.setLoginValidation(false);
+            }
         }
     };
     usersService.getAll();
