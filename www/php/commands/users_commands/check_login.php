d247bef6c4cd0d7e912d398f0d42139fb14d6cf0
Dmitry <zimokk1@gmail.com>
2016-07-16 18:28:16 +0200

    Added check login & email server-side methods

---
+++ www/php/commands/users_commands/check_login.php
@@ -0,0 +1,48 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+header("Content-Type: application/json; charset=UTF-8");
+
+
+
+// include database and object files
+
+include_once '../../config/database.php';
+
+include_once '../../objects/user.php';
+
+$data = json_decode(file_get_contents("php://input"));
+
+
+// instantiate database and product object
+$database = new Database();
+$db = $database->getConnection();
+
+// initialize object
+$user = new User($db);
+
+$login = $data->login;
+
+// query products
+$stmt = $user->check_login($login);
+$num = $stmt->rowCount();
+
+$data="";
+
+if($num>0){
+    $x=1;
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+        $data .= '{';
+            $data .= '"idUsers":"'  . $idUsers . '",';
+            $data .= '"login":"'  . $login . '",';
+            $data .= '"first_name":"'  . $first_name . '",';
+            $data .= '"last_name":"'  . $last_name . '",';
+            $data .= '"phone":"'  . $phone . '",';
+            $data .= '"email":"'  . $email . '"';
+        $data .= '}';
+        $data .= $x<$num ? ',' : ''; $x++; }
+}
+echo '{"users":[' . $data . ']}';
+?>
--- www/php/objects/user.php
+++ www/php/objects/user.php
@@ -1,50 +1,46 @@
 <?php
+
 class User{
-    // database connection and table name
     private $conn;
     private $table_name = "users";
 
-    // object properties
     public $idUser;
     public $login;
     public $password;
     public $first_name;
     public $last_name;
     public $phone;
-    public $mail;
+    public $email;
 
-    // constructor with $db as database connection
     public function __construct($db){
         $this->conn = $db;
     }
-
-    // read users
     function readAll(){
-
-        // select all query
         $query = "SELECT * FROM " . $this->table_name . ";";
-
-        // prepare query statement
         $stmt = $this->conn->prepare( $query );
-
-        // execute query
         $stmt->execute();
-
         return $stmt;
     }
-
     function authorize($login, $password){
         $query = "SELECT * FROM " . $this->table_name . " WHERE login=:login and password=:password ;";
-
-        // prepare query statement
         $stmt = $this->conn->prepare( $query );
-
         $stmt->bindParam(":login", $login);
         $stmt->bindParam(":password", $password);
-
-        // execute query
         $stmt->execute();
-
+        return $stmt;
+    }
+    function check_email($email){
+        $query = "SELECT * FROM " . $this->table_name . " WHERE email=:email;";
+        $stmt = $this->conn->prepare( $query );
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+        return $stmt;
+    }
+    function check_login($login){
+        $query = "SELECT * FROM " . $this->table_name . " WHERE login=:login;";
+        $stmt = $this->conn->prepare( $query );
+        $stmt->bindParam(":login", $login);
+        $stmt->execute();
         return $stmt;
     }
 }
