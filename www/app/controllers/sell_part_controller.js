2e0948c7521b61e06e0b003ee5fc6f9ec9ef7cce
Dmitry <zimokk1@gmail.com>
2016-07-26 22:10:33 +0200

    Added sell-part page

--- www/app/controllers/sell_part_controller.js
+++ www/app/controllers/sell_part_controller.js
@@ -1,3 +1,14 @@
-app.controller('SellPartCtrl', ['$scope',function($scope) {
-
+app.controller('SellPartCtrl', ['$scope','Models','Marks', 'Countries', 'Regions','Cities',
+                function($scope,Models,Marks,Countries,Regions,Cities) {
+    $scope.Models = Models.all;
+    $scope.Marks = Marks.all;
+    $scope.Countries = Countries.all;
+    $scope.Regions = Regions.all;
+    $scope.Cities = Cities.all;
+    $scope.updateRegions = function () {
+        Regions.getByCountry($scope.country_id);
+    };
+    $scope.updateCities = function () {
+        Cities.getByRegion($scope.region_id);
+    };
 }]);
