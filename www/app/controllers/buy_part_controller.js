9bfe5e695c4585ed4a9ea6dea37878d1b538781f
Dmitry <zimokk1@gmail.com>
2016-07-27 01:15:02 +0200

    Edited buy part page

--- www/app/controllers/buy_part_controller.js
+++ www/app/controllers/buy_part_controller.js
@@ -1,3 +1,17 @@
-app.controller('BuyPartCtrl', ['$scope',function($scope) {
-
+app.controller('BuyPartCtrl', ['$scope','Models','Marks', 'Countries', 'Regions','Cities',
+            function($scope,Models,Marks,Countries,Regions,Cities) {
+    $scope.Models = Models.all;
+    $scope.Marks = Marks.all;
+    $scope.Countries = Countries.all;
+    $scope.Regions = Regions.all;
+    $scope.Cities = Cities.all;
+    $scope.updateRegions = function () {
+        Regions.getByCountry($scope.country_id);
+    };
+    $scope.updateCities = function () {
+        Cities.getByRegion($scope.region_id);
+    };
+    $scope.changeMark = function(mark_id){
+        $scope.mark_id = mark_id;
+    };
 }]);
